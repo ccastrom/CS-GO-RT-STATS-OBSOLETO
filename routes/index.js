ac557711187eaf050a9207a3000718be491868bc
BRAVOSIX <claudio_jr@live.cl>
2022-04-05 16:02:21 -0400

    Added bootstrap to the project and the landing page to 'index.ejs'

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,21 @@ const express = require('express');
 const ruta=express.Router();
 const User= require('../models/user_model');
 
+var steam = require('steam-web');
+var kills;
+var stats;
+
+var s = new steam({
+  apiKey: '5DC20E24D2E76A091F52A43BCCBFA67A',
+  format: 'json' //optional ['json', 'xml', 'vdf']
+});
+
+
+
+ruta.use(express.static('views'));
+ruta.use(express.static('views/bootstrap'));
+ruta.use(express.static('node_modules/bootstrap/dist/js'));
+ruta.use(express.static('public/images'));
 
 
 
@@ -11,11 +26,28 @@ ruta.get('/',async(req,res)=>{
    //let resultado=selectDataUser();
    res.render('index', { user: req.user });
 
+   if(req.user==null){
+     console.log("nada")
+   }else{
+    s.getUserStatsForGame({
+      steamid: req.user.id,
+      appid: 730,
+      callback: function(err,data) {
+        kills=data.playerstats.stats[0].value
+        console.log(data.playerstats.stats[0].value);
+      }
+    })
+   }
+   
+   
+
    
 });
 
 ruta.get('/account', ensureAuthenticated, function(req, res){
-  res.render('account', { user: req.user });
+
+  res.render('account',  { user: req.user, stats:kills });
+
 });
 
 ruta.get('/logout', function(req, res){
