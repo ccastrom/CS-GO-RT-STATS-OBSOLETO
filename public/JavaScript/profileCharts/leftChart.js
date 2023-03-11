30e6bd21bffbf20a565cd520681a298b53cd3637
BRAVOSIX <claudio_jr@live.cl>
2023-03-11 15:27:41 -0300

    Working in frontend

--- public/JavaScript/profileCharts/leftChart.js
+++ public/JavaScript/profileCharts/leftChart.js
@@ -27,9 +27,18 @@
       label: 'My First Dataset',
       data: [killsFromEJS, deathsFromEJS],
       backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
+        'rgba(0, 207, 222,0.5)',
+        'rgba(206, 41, 41,0.5)',
+       
       ],
+      borderColor:[
+        'rgb(0, 207, 222)',
+        'rgb(239, 41, 41)',
+       
+      
+    ],
+    borderWith:1,
+    borderRadius:1,
       hoverOffset: 4,
       cutout:'85%'
     }]
@@ -41,14 +50,14 @@
       const {ctx,chartArea:{left,right,top,bottom,width,height}}=chart;
       ctx.save();
       ctx.font='bolder 30px Arial';
-      ctx.fillStyle='rgb(255, 99, 132)';
-      ctx.fillText(killsAndDeathsValueTruncated,140,170)
+      ctx.fillStyle='rgb(235, 235, 235)';
+      ctx.fillText(killsAndDeathsValueTruncated,120,160)
       ctx.restore();
     
 
       ctx.font='bolder 20px Arial';
-      ctx.fillStyle='rgb(255, 99, 132)';
-      ctx.fillText('K/D',154,230)
+      ctx.fillStyle='rgb(235, 235, 235)';
+      ctx.fillText('K/D',135,205)
       ctx.restore();
       
     }
--- public/JavaScript/profileCharts/middleChart.js
+++ public/JavaScript/profileCharts/middleChart.js
@@ -1,37 +1,67 @@
+var chart    = document.getElementById('MyChart1').getContext('2d'),
+    gradient = chart.createLinearGradient(0, 0, 0, 450);
+
+gradient.addColorStop(0, 'rgba(0, 199, 214, 0.32)');
+gradient.addColorStop(0.3, 'rgba(0, 199, 214, 0.1)');
+gradient.addColorStop(1, 'rgba(0, 199, 214, 0)');
 
 
-
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-  ];
-
-  const data = {
-    labels: labels,
+var data  = {
+    labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October', 'November','December' ],
     datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45],
+			label: 'Applications',
+			backgroundColor: gradient,
+			pointBackgroundColor: '#00c7d6',
+			tension:0.4,
+      fill:true,
+			borderColor: '#0e1a2f',
+      borderWidth:1,
+			data: [60, 45, 80, 30, 35, 55,25,80,40,50,80,50]
     }]
-  };
+};
 
-  const config = {
+var options = {
+	responsive: true,
+	maintainAspectRatio: true,
+	animation: {
+		easing: 'easeInOutQuad',
+		duration: 520
+	},
+	scales: {
+		y: {
+      ticks: {
+        color: '#5e6a81'
+      },
+			grid: {
+				color: 'rgba(226, 222, 222, 0.09)',
+        lineWidth: 1
+				
+			}
+		},
+    x:{
+      ticks: {
+        color: '#5e6a81'
+      }
+    },
+  
+    
+    
+	},
+	
+	legend: {
+		display: false
+	},
+	point: {
+		backgroundColor: '#00c7d6'
+	},
+
+};
+
+var chartInstance = new Chart(chart, {
     type: 'line',
     data: data,
-    options: {}
-  };
-
-  const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
-
-
+		options: options
+});
 
 
 
